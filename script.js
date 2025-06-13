ecad20efde07eb137becbfc786bedfb6d7d6265b
Ali Mert <68358873+alikral2618@users.noreply.github.com>
2025-06-13 19:46:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const startDate = new Date("2023-02-14"); // İlk çıktığınız tarih
+const startDate = new Date("2023-02-14"); // İlk tanışma ya da sevgili olma tarihi
 const today = new Date();
 const diffTime = today - startDate;
 const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
