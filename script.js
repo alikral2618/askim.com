1d1a137af96a59147ec6d71afa3e5df49a34c505
Ali Mert <68358873+alikral2618@users.noreply.github.com>
2025-06-13 19:42:38 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const startDate = new Date("2023-02-14"); // İlk çıktığınız tarih
+const today = new Date();
+const diffTime = today - startDate;
+const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+document.getElementById("day-counter").textContent = diffDays;
